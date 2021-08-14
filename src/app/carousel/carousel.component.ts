eaff10f748e1f0d75d89928d15eb13f790613755
rafiulratul21 <rafiulratul21@gmail.com>
2021-08-14 13:27:32 +0000

    carousel ready. needs refactoring

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
+
 export class CarouselComponent implements OnInit {
+  carouselInterval = 2000;
+  activeSlideIndex = 0;
+  slides: { image: string }[] = [
+    { image: "assets/images/carousel/1.jpg" },
+    { image: "assets/images/carousel/2.jpg" },
+  ]
 
   constructor() { }
 
